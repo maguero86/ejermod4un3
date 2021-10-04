130bdc6ae4f1404607fd8e5faedda09cf503a484
maguero86 <86382159+maguero86@users.noreply.github.com>
2021-10-04 18:04:11 -0300

    Modificado para esperar un parametro
    Modificado para esperar un parametro

--- nosotros.js
+++ nosotros.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next){
+/*router.get('/', function(req, res, next){
 	res.send('Hago un GET');
-});
+});*/
+
+router.get('/', function(req, res, next){
+	if(req.query.perfil == 'admin')
+		res.send('Hola usuario ADMINISTRADOR');
+	if(req.query.perfil == 'dev')
+		res.send('Hola usuario DESARROLLADOR');
+}); 
 
 router.post('/', function(req, res, next){
 	res.send('Hago un POST');
